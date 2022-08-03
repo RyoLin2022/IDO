890bb4ba969f88ad42ccf8ef6aae51520f6493e2
Ryo <samxyz258456@gmail.com>
2022-08-04 03:22:17 +0800

    StakeOnChain

--- src/Pages/Staking.js
+++ src/Pages/Staking.js
@@ -34,9 +34,9 @@ let currentAccount = null;
 
 function Staking() {
 
+  var tokenDecimal = 6;
   let contractAddress = "0xA20DF0188F1330E1c80e012901735B9C1b58E27a";       //Modify Contract Address here!!
   let stakingContractAddress = "0xb26318a92f465004f343562D649D9908D8fE5B03";//Modify Staking Contract Address here!!
-
 
   /*------------------Here's the Balance for the staking contract-----------------*/
   /*------------------Here's the Balance for the staking contract-----------------*/
@@ -56,7 +56,6 @@ function Staking() {
     });
     var balanceDEC = Number(accBalance).toString(10);
 
-    var tokenDecimal = 6;
     var inWeiBal = balanceDEC.length;
     var CAbalance = document.getElementById("StakingContractTokenBalance");
 
@@ -92,9 +91,11 @@ function Staking() {
 
     if (accAllowNum > 0) {
       document.getElementById("Approve-btn").hidden = true;
+      document.getElementById("Unstake-btn").hidden = false;
       document.getElementById("Stake-btn").hidden = false;
     } else {
       document.getElementById("Approve-btn").hidden = false;
+      document.getElementById("Unstake-btn").hidden = true;
       document.getElementById("Stake-btn").hidden = true;
     }
   }
@@ -118,7 +119,6 @@ function Staking() {
       ]
     });
     var balanceDEC = Number(accBalance).toString(10);
-    var tokenDecimal = 6;
     var inWeiBal = balanceDEC.length;
     var CAbalance = document.getElementById("StakingAccountBalance");
 
@@ -128,9 +128,9 @@ function Staking() {
   }
 
 
-  /*------------------Here's the Staking Balance for certain account-----------------*/
-  /*------------------Here's the Staking Balance for certain account-----------------*/
-  /*------------------Here's the Staking Balance for certain account-----------------*/
+  /*------------------Here's the Staking Reward for certain account-----------------*/
+  /*------------------Here's the Staking Reward for certain account-----------------*/
+  /*------------------Here's the Staking Reward for certain account-----------------*/
   async function ACCStakingReward() {
     let inputData = "0x4d318018000000000000000000000000" + currentAccount.substring(2, currentAccount.length);
     let accBalance = await window.ethereum.request({
@@ -144,7 +144,6 @@ function Staking() {
       ]
     });
     var balanceDEC = Number(accBalance).toString(10);
-    var tokenDecimal = 6;
     var inWeiBal = balanceDEC.length;
     var CAbalance = document.getElementById("StakingAccountInterest");
 
@@ -153,40 +152,87 @@ function Staking() {
     CAbalance.innerText = rounded;
   }
 
-  // async function ACCerc20Balance() {    
-  //   let inputData = "0x70a08231000000000000000000000000" + currentAccount.substring(2, currentAccount.length);
-  //   let accBalance = await window.ethereum.request({
-  //     method: "eth_call",
-  //     params: [{
-  //       to: contractAddress,
-  //       data: inputData,
-  //       //BalanceOf:0x70a08231
-  //       //(account):0000000000000000000000006b25Cb9338b4cEC5632aFd12B905C9C25a71BB4b
-  //     },
-  //       "latest"
-  //     ]
-  //   });
-  //   var balanceDEC = Number(accBalance).toString(10);
-  //   console.log(balanceDEC);
-  //   var tokenDecimal = 6;
-  //   var inWeiBal = balanceDEC.length;
-  //   var CAbalance = document.getElementById("StakingAccountBalance");
-  //   var str = Math.pow(10, (inWeiBal - tokenDecimal - 4));
-  //   var rounded = Math.round(str * parseInt(balanceDEC.substring(0, 4)) * 10000) / 10000;
-  //   CAbalance.innerText = rounded;
-  // }
+  async function ACCerc20Balance() {    
+    let inputData = "0x70a08231000000000000000000000000" + currentAccount.substring(2, currentAccount.length);
+    let accBalance = await window.ethereum.request({
+      method: "eth_call",
+      params: [{
+        to: contractAddress,
+        data: inputData,
+        //BalanceOf:0x70a08231
+        //(account):0000000000000000000000006b25Cb9338b4cEC5632aFd12B905C9C25a71BB4b
+      },
+        "latest"
+      ]
+    });
+    var balanceDEC = Number(accBalance).toString(10);
+    console.log(balanceDEC);
+    var inWeiBal = balanceDEC.length;
+    var CAbalance = document.getElementById("ACCTokenBalance");
+    var str = Math.pow(10, (inWeiBal - tokenDecimal - 4));
+    var rounded = Math.round(str * parseInt(balanceDEC.substring(0, 4)) * 10000) / 10000;
+    CAbalance.innerText = rounded;
+  }
+
+
+async function makeStake() {
+  let inputValueDEC = document.getElementById("stakeMiddle").value;
+  let inputValueHex = Number(inputValueDEC).toString(16);
+
+  let Zeros = Math.pow(10,20-inputValueHex.length);
+  let stringZeros = Zeros.toString();
+
+  let inputData="0xadf8ccaa"+"00000000000000000000000000000000000000000000"
+  +stringZeros.substring(1,Zeros.length)+inputValueHex;
+  console.log(inputData);
+  //0xadf8ccaa0000000000000000000000000000000000000000000000000000000000989680
+  //0xadf8ccaa0000000000000000000000000000000000000000000000000000000000989680
+  let inputGasPrice = await window.ethereum.request({
+    method: "eth_gasPrice"
+  });
+
+  let params = [
+    {
+      from: currentAccount,
+      to: stakingContractAddress,
+      gas: Number(300000).toString(16), // 30400
+      gasPrice: inputGasPrice, // 
+      value: 0,
+      data:inputData,
+    },
+  ]
+
+  //Result is the transaction hash
+  let result = await window.ethereum.request({ method: "eth_sendTransaction", params }).catch((err) => {
+    console.log(err);
+  })
+
+  if (result) {
+    var TXSent = document.getElementById("GoingToStake");
+    TXSent.innerText = "Transaction has been sent";
+    console.log(result);
+  }
+  setTimeout(function () {
+    console.log("The first log delay 10 second");
+  }, 20000);
+}
+
 
 
   /*------------------Here's the token Approval-----------------*/
   /*------------------Here's the token Approval-----------------*/
   /*------------------Here's the token Approval-----------------*/
   async function ApproveToken() {
+    let inputGasPrice = await window.ethereum.request({
+      method: "eth_gasPrice"
+    });
+  
     let params = [
       {
         from: currentAccount,
         to: contractAddress,
         gas: Number(100000).toString(16), // 30400
-        gasPrice: Number(10000000000).toString(16), // 10000000000
+        gasPrice: inputGasPrice, // 10000000000
         value: '0', // 2441406250
         data: '0x095ea7b3000000000000000000000000b26318a92f465004f343562d649d9908d8fe5b030000000000000000000000000000000000000000204fce5e3e25026110000000',
 
@@ -194,7 +240,6 @@ function Staking() {
       },
     ];
 
-    var TokenApprove = document.getElementById("Approve-btn");
     let result = window.ethereum
       .request({
         method: 'eth_sendTransaction',
@@ -247,7 +292,7 @@ function Staking() {
 
     var str = Math.pow(10, (inWeiBal - 22));
     var rounded = Math.round(str * parseInt(balanceDEC.substring(0, 4)) * 10000) / 10000;
-    balanceBtn.innerText = rounded + " BNB";
+    balanceBtn.innerText = rounded + " OKT";
   }
 
   async function connectWallet() {
@@ -266,6 +311,7 @@ function Staking() {
       ACCStakingReward();
       getBalance();
       ACCAllowance();
+      ACCerc20Balance();
       alert("Wallet connected successfully!");
     } else {
       alert("Please install an injected Web3 wallet");
@@ -275,17 +321,34 @@ function Staking() {
 
   const [style, setStyle] = useState("overlay");
   
-  const OpenModal = () => {
-    console.log("you just clicked");
-  
+  const OpenStake = () => {  
     setStyle("overlay2");
   };  
 
-  const CloseModal = () => {
-    console.log("you just clicked");
-  
+  const CloseStake = () => {  
     setStyle("overlay");
   };
+
+  
+  const [style2, setStyle2] = useState("overlay3");
+  
+  const OpenUnstake = () => {  
+    setStyle2("overlay4");
+  };  
+
+  const CloseUnstake = () => {  
+    setStyle2("overlay3");
+  };
+
+  function getStakeAmount() {
+    let staking = document.getElementById("stakeAmountID");
+    let inputValue = staking.value;
+    let inputValueDecimal = inputValue*Math.pow(10,tokenDecimal);    
+    document.getElementById("stakeMiddle").value=inputValueDecimal;
+   
+    makeStake();
+    staking.value = null;
+  }
 
 
 
@@ -309,14 +372,14 @@ function Staking() {
           </div>
           <br />
           <table id='APR'>
-            <tr>
+            <tr id='layer1'>
               <td id='topleft'>Annul Percentage Yield</td>
-              <td>365%</td>
+              <td className="topRight">365%</td>
             </tr>
             <br />
-            <tr>
-              <td id='topleft'>Total Staked</td><br />
-              <td id="StakingContractTokenBalance">0.0</td>
+            <tr id='layer2'>
+              <td id='topleft'>Total Staked</td>
+              <td id="StakingContractTokenBalance" className="topRight">0.0</td>
             </tr>
 
           </table>
@@ -336,14 +399,30 @@ function Staking() {
         <div className="staking-box-4">
           <button id="Approve-btn" onClick={ApproveToken}>Approve</button>
 
-          <div className={style} id="overlay">
+          <div className={style}>
             <div className="popup">
-              <div onClick={CloseModal} className="CloseIcon">X</div>
+              <div onClick={CloseStake} className="CloseIcon">X</div>
               <h3>Stake</h3>
+              <h3>Balance<span id="ACCTokenBalance"></span></h3>
+              <input className="stakeAmountClass" id="stakeAmountID"></input>
+              <button id="GoingToStake" onClick={getStakeAmount}>Stake Now</button>
             </div>
           </div>
+
           
-          <button id = "Stake-btn" onClick={OpenModal} hidden>Stake</button>
+          <div className={style2}>
+            <div className="popup2">
+              <div onClick={CloseUnstake} className="CloseIcon">X</div>
+              <h3>Unstake</h3>
+            </div>
+          </div>
+
+          <div className="ToStakeOrNotToStake">
+            <button id = "Stake-btn" onClick={OpenStake} hidden>+</button>
+            <div id ="stakeMiddle"></div>
+            <button id = "Unstake-btn" onClick={OpenUnstake} hidden>-</button>
+          </div>
+          {/* <button onClick={ethEstimateGas}>Test</button> */}
         </div>
       </div>
     </div>
